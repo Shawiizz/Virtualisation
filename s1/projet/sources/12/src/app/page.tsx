b647e39b40f2639e1cfa761aeca5e7168acddb92
Shawiizz <shaweereso@gmail.com>
2024-09-23 10:41:48 +0200

    ajout du projet

---
+++ s1/projet/sources/12/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen w-screen">
+      <h1 className="animate-spin text-5xl">AAAAAAAAAAAAAA</h1>
+    </div>
+  );
+}
